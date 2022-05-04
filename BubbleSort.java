b4f22fcf23502c34044243051947725e5b25f9c1
iyogeshmore <iyogesh.more1998@gmail.com>
2022-05-04 12:40:27 +0530

    Added Bubbble sort program

---
+++ BubbleSort.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.day16and17;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args)
+    {
+        int i,j,temp=0;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter how many numbers to be sorted:");
+        int n=scanner.nextInt();
+        int a[]=new int[n];
+        System.out.println();
+        System.out.println("Enter value for "+n+" numbers:");
+
+        for(i=0;i<a.length;i++)
+        {
+            //taking input from user
+            a[i]=scanner.nextInt();
+        }
+
+        for(i=0;i<a.length;i++)         //sorting the array
+        {
+            for(j=i+1;j<a.length;j++)
+            {
+                if(a[i]>a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("The sorted order is:");
+
+        for(i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);		//printing the sorted array
+        }
+    }
+}
